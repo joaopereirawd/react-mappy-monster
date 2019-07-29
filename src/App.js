4aed6d21f83053139c8be1dcaf354b23c58beca1
João Pereira <joaopereirawd@gmail.com>
2019-07-29 18:06:11 +0100

    upd: gh-pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './img/logo.svg';
 import './App.css';
-
+import './scss/header.scss';
+import './scss/hero.scss';
+import './scss/page.scss';
+import map from './img/map.svg';
+import mappy from './img/monster.svg';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className="header">
+            <div className="row">
+                <div className="logo">
+                    <img src={logo}></img> 
+                </div>
+            </div>
+        </header>
+        <section className="heroshot">
+            <div className="hero-graphics">
+                  <div className="mappy">
+                    <img src={mappy}></img> 
+                  </div>
+                  <div className="map">
+                      <img src={map}></img> 
+                  </div>
+            </div>
+        </section>
+        <section className="page-content">
+            <div className="row"> 
+                <div className="caption">
+                    <h1><span>react-mappy-monster</span> is a react.js component blah blah blah</h1> 
+                </div>
+                <div className="iconset">
+                    <ul>
+                      <li>1</li>
+                      <li>2</li>
+                      <li>3</li>
+                    </ul>
+                </div>
+            </div>
+        </section>
     </div>
   );
 }
