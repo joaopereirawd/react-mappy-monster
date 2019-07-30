e63a17eeb4a040964410a98f8ab1eac1cda6fec1
João Pereira <joaopereirawd@gmail.com>
2019-07-30 14:28:17 +0100

    Updates

---
+++ precache-manifest.ac28a28d708db0b74bed41ac8b55b6b4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b032b9817da629997001a91b18b1e670",
+    "url": "/react-mappy-monster/index.html"
+  },
+  {
+    "revision": "cf97328dbd1f05606e79",
+    "url": "/react-mappy-monster/static/css/main.6c79fd30.chunk.css"
+  },
+  {
+    "revision": "2b9d2c265d18e8fd08d2",
+    "url": "/react-mappy-monster/static/js/2.42c6d73e.chunk.js"
+  },
+  {
+    "revision": "cf97328dbd1f05606e79",
+    "url": "/react-mappy-monster/static/js/main.e5843095.chunk.js"
+  },
+  {
+    "revision": "6393ade7a6f12f52ad6e",
+    "url": "/react-mappy-monster/static/js/runtime~main.3a26ed9b.js"
+  },
+  {
+    "revision": "66f9c61ff4ff0589272ccc48673594cf",
+    "url": "/react-mappy-monster/static/media/icon-design.66f9c61f.svg"
+  },
+  {
+    "revision": "5f64a793a485277356908fb7969276dd",
+    "url": "/react-mappy-monster/static/media/icon-google.5f64a793.svg"
+  },
+  {
+    "revision": "60069f48cb9203cce9172cee7b71231e",
+    "url": "/react-mappy-monster/static/media/icon-relax.60069f48.svg"
+  },
+  {
+    "revision": "a6b857bf1b1236988ad3c2aa2be75faa",
+    "url": "/react-mappy-monster/static/media/logo.a6b857bf.svg"
+  },
+  {
+    "revision": "36109dff89cbb8cd8b2d47e35f2cb4bf",
+    "url": "/react-mappy-monster/static/media/map.36109dff.svg"
+  },
+  {
+    "revision": "a4e613295014674dacc60f33e70aeeed",
+    "url": "/react-mappy-monster/static/media/monster.a4e61329.png"
+  }
+]);
