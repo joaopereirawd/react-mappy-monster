6a110665910b47b9830a5967cd584f308deab291
João Pereira <joaopereirawd@gmail.com>
2019-08-02 15:00:51 +0100

    upd: build

---
+++ build/precache-manifest.fbb2ce92f11db6ed1ddb46a31d339ea0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "392439d3e12dd015fabbfe5b45dffa88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e340539bf340cb6bb58c",
+    "url": "/static/css/main.b8933d6a.chunk.css"
+  },
+  {
+    "revision": "cf7bd6f28a8f3849a7e5",
+    "url": "/static/js/2.c3056a2a.chunk.js"
+  },
+  {
+    "revision": "e340539bf340cb6bb58c",
+    "url": "/static/js/main.f8565c8a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "66f9c61ff4ff0589272ccc48673594cf",
+    "url": "/static/media/icon-design.66f9c61f.svg"
+  },
+  {
+    "revision": "5f64a793a485277356908fb7969276dd",
+    "url": "/static/media/icon-google.5f64a793.svg"
+  },
+  {
+    "revision": "60069f48cb9203cce9172cee7b71231e",
+    "url": "/static/media/icon-relax.60069f48.svg"
+  },
+  {
+    "revision": "7e7260c43a8aeb1acee583bf360559f9",
+    "url": "/static/media/logo.7e7260c4.svg"
+  },
+  {
+    "revision": "1570daee999024d32bd5b8b4dfc892ef",
+    "url": "/static/media/map.1570daee.svg"
+  },
+  {
+    "revision": "a4e613295014674dacc60f33e70aeeed",
+    "url": "/static/media/monster.a4e61329.png"
+  }
+]);
