bbdcd10bd891ff78125d52c4167d3325c898752f
João Pereira <joaopereirawd@gmail.com>
2019-08-02 15:17:31 +0100

    house cleaning

--- src/index.js
+++ src/index.js
@@ -123,8 +123,6 @@ export class Map extends React.Component {
             s.src = `https://maps.google.com/maps/api/js?key=${this.props.apiKey ? this.props.apiKey : '' }`;
             var x = document.getElementsByTagName('script')[0];
             x.parentNode.insertBefore(s, x);
-            // Below is important. 
-            //We cannot access google.maps until it's finished loading
             s.addEventListener('load', e => {
                 this.gScript();
             })
